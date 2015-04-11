e87707cd4f39f4a2ee761cbb1375e973078ee481
Martin Gercsak <Martin_Gercsak@amp.com.au>
2015-04-11 18:41:45 +1000

    configured freemarker

--- dsl-generator/src/main/java/org/marting/Main.java
+++ dsl-generator/src/main/java/org/marting/Main.java
@@ -1,24 +1,57 @@
 package org.marting;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
 public final class Main {
 
 	private Main() { }
 
-	public static void main(String[] argv) {
+	public static void main(String[] args) {
+		readInputParameters(args);
+        //Freemarker configuration object
+        Configuration cfg = new Configuration();
+        cfg.setClassForTemplateLoading(Main.class, "/");
+        try {
+            //Load template from source folder
+            Template template = cfg.getTemplate("templates/dsl-template.ftl");
+             
+            // Build the data-model
+            Map<String, Object> data = new HashMap<String, Object>();
+            data.put("dslClassName", "TestDSL");
+            
+            // Console output
+            Writer out = new OutputStreamWriter(System.out);
+            template.process(data, out);
+            out.flush();
+             
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+		
+	}
 
+	private static void readInputParameters(String[] args) {
 		CommandLineParser parser = new BasicParser();
 		Options options = new Options();
 		options.addOption("a", "all", false, "do not hide entries starting with .");
 		options.addOption("A", "almost-all", false, "do not list implied . and ..");
 		options.addOption("b", "escape", true, "print octal escapes for nongraphic ");
-
-		String[] args = new String[] {"-b", "xyz"};
 
 		try {
 			// parse the command line arguments
